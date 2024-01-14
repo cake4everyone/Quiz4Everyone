7d8399f0107ad62365fcbd2e7e294ac026eec981
Niraculix <75939533+Niraculix@users.noreply.github.com>
2024-01-14 15:14:52 +0100

    HTTP Login request

--- Webserver.gd
+++ Webserver.gd
@@ -6,10 +6,48 @@ var token = ""
 
 signal StartQuiz
 
+
+# INNATE FUNC
+
 func _ready():
+	load_config()
+	$LoginHTTP.request_completed.connect(_on_http_request_completed)
 	$QuestionScene.hide()
 	$Start.hide()
-	load_config()
+	
+	
+func _process(delta):
+	if wsrunning:
+		ws.poll()
+		if ws.get_ready_state() == WebSocketPeer.STATE_CONNECTING:
+			return
+
+		if ws.get_ready_state() != WebSocketPeer.STATE_OPEN:
+			print("connection lost")
+			set_process(false)
+			return
+
+	if(Input.is_action_just_pressed("ui_accept") and $Password.text != ""):
+		token = $Password.text
+		$Password.text = ""
+		$Start.show()
+		var login_dict = {
+			"password" = "TotsBIOQmtGZheiUaQLg2xvM9",
+			"twitch" = token,
+		}
+		var json_string = JSON.stringify(login_dict)
+		$LoginHTTP.request("http://" + host + "/login", [], HTTPClient.METHOD_POST, json_string)
+		connect_to_ws()
+		
+	receive()
+
+
+func load_config():
+	var file = FileAccess.open("res://config.yaml", FileAccess.READ)
+	host = file.get_line()
+
+
+# WEBSOCKET
 
 func connect_to_ws():
 	var headers = PackedStringArray(["Authorization: Q4E "+token])
@@ -29,39 +67,32 @@ func _send_message_to_ws(message : String):
 		return
 	print("WS <= ", message)
 
-func _process(delta):
-	if wsrunning:
-		ws.poll()
-		if ws.get_ready_state() == WebSocketPeer.STATE_CONNECTING:
-			return
-
-		if ws.get_ready_state() != WebSocketPeer.STATE_OPEN:
-			print("connection lost")
-			set_process(false)
-			return
-
-	if(Input.is_action_just_pressed("ui_accept") and $Password.text != ""):
-		token = $Password.text
-		$Password.text = ""
-		$Start.show()
-		connect_to_ws()
-	receive()
-
-func load_config():
-	var file = FileAccess.open("res://config.yaml", FileAccess.READ)
-	host = file.get_line()
-
-func _on_button_pressed():
-	await get_tree().create_timer(1).timeout
-	get_tree().quit()
 
 func receive():
 	while ws.get_available_packet_count():
 		var received_data = ws.get_packet()
 		print("WS => ", received_data.get_string_from_ascii())
 
+
+# USER INTERFACE
+
+func _on_button_pressed():
+	await get_tree().create_timer(1).timeout
+	get_tree().quit()
+
 func _on_start_pressed():
 	StartQuiz.emit()
 	$Password.hide()
 	_send_message_to_ws("GAME: Test")
 	await get_tree().create_timer(1).timeout
+
+
+#HTTP
+
+func _on_http_request_completed(result, response_code, headers, body):
+	print("test ", response_code)
+	print(body.get_string_from_ascii())
+
+
+
+
