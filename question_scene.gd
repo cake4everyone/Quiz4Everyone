5463071324e80a7fb768fa8ef45c337f9c962e1e
Niraculix <75939533+Niraculix@users.noreply.github.com>
2024-01-21 20:12:45 +0100

    test question added

--- question_scene.gd
+++ question_scene.gd
@@ -1,10 +1,26 @@
 extends Node2D
 
+var quizOn = false
+var streamerAnswer = ""
 
 func _on_main_start_quiz():
 	self.show()
+	quizOn = true
 	load_data()
+	
 
 
 func load_data():
 	pass
+
+func _process(delta):
+	if(quizOn == true && streamerAnswer == ""):
+		if(Input.is_action_just_pressed("AnswerA")):
+			streamerAnswer = "A"
+		elif(Input.is_action_just_pressed("AnswerB")):
+			streamerAnswer = "B"
+		elif(Input.is_action_just_pressed("AnswerC")):
+			streamerAnswer = "C"
+		elif(Input.is_action_just_pressed("AnswerD")):
+			streamerAnswer = "D"
+	$Label.text = streamerAnswer
