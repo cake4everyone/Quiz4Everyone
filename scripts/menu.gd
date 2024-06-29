bc156183342059a4ba25d5342268d8c28825ae8a
Niraculix <75939533+Niraculix@users.noreply.github.com>
2024-06-29 14:38:00 +0200

    Main Menu Overhaul

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -5,10 +5,10 @@ var round_duration: int
 
 ## ready is called when the node enters the scene tree for the first time.
 func _ready():
-	$ModeSelect.add_item("Streamer VS Chat", 0)
+	$RoundCreation/CreationMenu/ModeSelect.add_item("Streamer VS Chat", 0)
 	if (scene_manager.round_duration > 0):
 		round_duration = scene_manager.round_duration
-		$RoundDuration.value = round_duration
+		$RoundCreation/CreationMenu/RoundDuration.value = round_duration
 	api.category(on_category_response)
 
 ## on_category_response is called as when the categories api call completed.
@@ -20,33 +20,45 @@ func on_category_response(success: bool, categories: Dictionary={}):
 
 ## update_category_list updates the category selection list with the given dictionary (mapping String to int).
 func update_category_list(categories: Dictionary):
+	const copy_category = preload("res://scenes/Objects/copy_category.tscn")
 	for category in categories:
-		var c: HBoxContainer = $CopyCategory.duplicate() # # TODO: create new categories via code instread of copying a template from the tree
+		var c = copy_category.instantiate()
 		c.get_child(0).max_value = categories[category]
 		c.get_child(1).text = category
 		c.show()
-		$CategoryBox/Categories.add_child(c)
+		$RoundCreation/CreationMenu/CategoryBox/Categories.add_child(c)
 
 ## on_btn_start_pressed is called when pressed the start game button.
 ## It collects all the selected categories and creates a new game on the server.
 func on_btn_start_pressed():
-	$Start.disabled = true
-	round_duration = $RoundDuration.value
+	$RoundCreation/CreationMenu/Start.disabled = true
+	round_duration = $RoundCreation/CreationMenu/RoundDuration.value
 	var categories: Dictionary = {}
-	for category: HBoxContainer in $CategoryBox/Categories.get_children():
+	for category: HBoxContainer in $RoundCreation/CreationMenu/CategoryBox/Categories.get_children():
 		var amount: int = category.get_child(0).value
 		if amount == 0:
 			continue
 		categories[category.get_child(1).text] = amount
-
+		
 	var game_data: Dictionary = {}
 	game_data["categories"] = categories
 	game_data["round_duration"] = round_duration
 	api.game_start(JSON.stringify(game_data), on_game_start_response)
+	
+func on_cancel_pressed():
+	$RoundCreation.hide()
 
 func on_game_start_response(success: bool):
 	if !success:
 		print("failed to create new game!")
-		$Start.disabled = false
+		$RoundCreation/CreationMenu/Start.disabled = false
 		return
 	scene_manager.change_scene(self, "question")
+
+
+func on_play_pressed():
+	$RoundCreation.show()
+
+
+func on_quit_pressed():
+	get_tree().quit()
