309d867ff4d3f7a537880f268840f3e879a796f2
Kesuaheli <kesuaheligame@gmail.com>
2024-09-07 20:09:23 +0200

    Changes style of category selection

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -12,8 +12,15 @@ func _ready():
 		$RoundCreation/CreationMenu/RoundDuration.value = round_duration
 	api.category(on_category_response)
 
+	var category_tree: Tree = $RoundCreation/CreationMenu/CategoryBox
+	category_tree.create_item()
+	category_tree.set_column_title(0, "Category")
+	category_tree.set_column_title(1, "Amount")
+	category_tree.set_column_expand(1, false)
+	category_tree.set_column_custom_minimum_width(1, 100)
+
 ## on_category_response is called as when the categories api call completed.
-func on_category_response(success: bool, categories: Dictionary={}):
+func on_category_response(success: bool, categories: Dictionary = {}):
 	if !success:
 		print("failed to get categories!")
 		return
@@ -22,18 +29,26 @@ func on_category_response(success: bool, categories: Dictionary={}):
 ## update_category_list updates the category selection list with the given dictionary (mapping String to int).
 func update_category_list(groups: Dictionary):
 	var category_tree: Tree = $RoundCreation/CreationMenu/CategoryBox
-	var tree_root: TreeItem = category_tree.create_item()
-	category_tree.set_column_title(0, "Category")
-	category_tree.set_column_title(1, "Amount")
+	var tree_root: TreeItem = category_tree.get_root()
 	for group in groups:
 		var tree_group: TreeItem = category_tree.create_item(tree_root)
 		tree_group.set_text(0, group)
+		tree_group.set_custom_bg_color(0, Color(0.23, 0.23, 0.23))
+		tree_group.set_custom_bg_color(1, Color(0.23, 0.23, 0.23))
+		var bg_light: bool = false
 		for cat in groups[group]:
 			var category: TreeItem = category_tree.create_item(tree_group)
 			category.set_text(0, cat.description)
 			category.set_cell_mode(1, TreeItem.CELL_MODE_RANGE)
 			category.set_range_config(1, 0, cat.count, 1)
 			category.set_editable(1, true)
+			if bg_light:
+				category.set_custom_bg_color(0, Color(0.15, 0.15, 0.15))
+				category.set_custom_bg_color(1, Color(0.15, 0.15, 0.15))
+			else:
+				category.set_custom_bg_color(0, Color(0.05, 0.05, 0.05))
+				category.set_custom_bg_color(1, Color(0.05, 0.05, 0.05))
+			bg_light = !bg_light
 
 			var category_data: CategoryData = CategoryData.new()
 			category_data.id = cat.title
